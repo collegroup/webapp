76094ff1aa0a593acfbb1ca9be86d36c74887106
Arthur Colle <arthur@collegroup.com>
2018-04-19 20:13:08 +0000

    Added signin page

--- lib/colle_group.ex
+++ lib/colle_group.ex
@@ -6,4 +6,8 @@ defmodule ColleGroup do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  defp check_services_up() do
+
+  end
 end
